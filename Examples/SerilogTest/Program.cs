6a2b9bdd04ad5c1776dfcd022a552aa3e5bfbad7
Tuan Nguyen <nvtuan.ssu@gmail.com>
2019-07-30 16:40:47 +0900

    Added SerilogTest

---
+++ Examples/SerilogTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using System;
+
+namespace SerilogTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+            Console.ReadKey();
+        }
+    }
+}
