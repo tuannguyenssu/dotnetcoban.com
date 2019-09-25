6264e0a67ac69bfc562d3ecb5e531b98f06c584d
Tuan Nguyen <nvtuan.ssu@gmail.com>
2019-09-25 13:45:09 +0700

    Added DataStructureAndAlgorithmTest

---
+++ Examples/DataStructureAndAlgorithmTest/DictionariesAndSetsTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace DictionariesAndSetsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("name", "Tuan Nguyen");
+            hashtable["age"] = 27;
+            hashtable.Add("age", 20);
+            foreach(var key in hashtable.Keys)
+            {
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
