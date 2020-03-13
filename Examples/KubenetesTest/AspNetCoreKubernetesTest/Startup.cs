be5d630fb31342c17d5030965ea9ed396813ad4f
Nguyen Van Tuan (KCN) <tuannv5@bagroup.vn>
2020-03-13 17:48:52 +0700

    Update AspNetCoreKubernetesTest

--- Examples/KubenetesTest/AspNetCoreKubernetesTest/Startup.cs
+++ Examples/KubenetesTest/AspNetCoreKubernetesTest/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -45,10 +47,10 @@ namespace AspNetCoreKubernetesTest
 
                 endpoints.MapGet("/info", async context =>
                 {
-                    var message = $"Host: {Environment.MachineName}\n" +
-                                  $"EnvironmentName: {env.EnvironmentName}\n" +
-                                  $"Secret value: {Configuration["Database:ConnectionString"]}";
-                    await context.Response.WriteAsync(message);
+                    var connection = context.Features.Get<IHttpConnectionFeature>();
+                    await context.Response.WriteAsync($"Listening IP: {connection.LocalIpAddress}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"Hostname: {Dns.GetHostName()}{Environment.NewLine}");
+                    await context.Response.WriteAsync($"EnvVar Configuration value: {Configuration["Database:ConnectionString"]}{Environment.NewLine}");
                 });
             });
         }
