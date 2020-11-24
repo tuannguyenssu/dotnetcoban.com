dfdbd0693574d0c003b9dcf559e37c66126ca403
tuannguyenssu <tuannguyen@soongsil.ac.kr>
2020-11-24 17:47:43 +0700

    Added ServiceStackRedisTest

---
+++ Examples/ServiceStackRedisTest/ImageData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceStackRedisTest
+{
+    public class ImageData
+    {
+        public int CustomerId { get; set; }
+        public string VehicleName { get; set; }
+        public DateTime CapturedTime { get; set; }
+    }
+}
---
+++ Examples/ServiceStackRedisTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ServiceStack.Redis;
+
+namespace ServiceStackRedisTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var redisConnectionString = $"redis://192.168.1.57:6379?db=15";
+
+            var redisClientManager = new PooledRedisClientManager(redisConnectionString);
+
+            var redisClient = await redisClientManager.GetClientAsync();
+            var options = new ImageDataDbOptions()
+            {
+                DbName = "Abc"
+            };
+
+            var db = new ImageDataDb(redisClient, options);
+            var data = new List<ImageData>()
+            {
+                new ImageData()
+                {
+                    CustomerId = 1010,
+                    VehicleName = "BKSTEST",
+                    CapturedTime = DateTime.Now
+                }
+            };
+
+            //await db.SaveAllAsync(data);
+
+            var items = await db.GetAllAsync();
+
+            Console.WriteLine(items.Count);
+            Console.WriteLine("Finished!");
+            Console.ReadKey();
+        }
+    }
+}
