e8d2c9381fcb97fd566e901fdcef59b68f880022
tuannguyenssu <tuannguyen@soongsil.ac.kr>
2020-12-03 14:12:25 +0700

    Added AspNetCoreCacheTest

---
+++ Examples/AspNetCoreCacheTest/MemoryCacheTest/AutoRefreshMemoryCache.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+namespace MemoryCacheTest
+{
+    public class AutoRefreshMemoryCache
+    {
+        private readonly int _refreshTimeInSeconds;
+        private readonly IMemoryCache _memoryCache;
+        private readonly ConcurrentDictionary<object, SemaphoreSlim> _locks = new ConcurrentDictionary<object, SemaphoreSlim>();
+
+        public AutoRefreshMemoryCache(IMemoryCache memoryCache, int refreshInSeconds = 1)
+        {
+            _memoryCache = memoryCache;
+            _refreshTimeInSeconds = refreshInSeconds;
+        }
+
+        public async Task<object> GetAsync(object key, Func<object, object> invalidateConfigure)
+        {
+            var certLock = _locks.GetOrAdd(key, k => new SemaphoreSlim(1, 1));
+            await certLock.WaitAsync();
+            try
+            {
+                if (!_memoryCache.TryGetValue(key, out var value))
+                {
+                    if (value == null)
+                    {
+                        value = invalidateConfigure(key);
+                        _memoryCache.Set(key, value, GetMemoryCacheEntryOptions(_refreshTimeInSeconds, invalidateConfigure));
+                    }
+                }
+
+                return value;
+            }
+            finally
+            {
+                certLock.Release();
+            }
+        }
+
+        private MemoryCacheEntryOptions GetMemoryCacheEntryOptions(int expireInSeconds, Func<object, object> invalidateConfigure)
+        {
+            var expirationTime = DateTime.Now.AddSeconds(expireInSeconds);
+            var expirationToken = new CancellationChangeToken(new CancellationTokenSource(TimeSpan.FromSeconds(expireInSeconds + .01)).Token);
+
+            var options = new MemoryCacheEntryOptions();
+            options.SetAbsoluteExpiration(expirationTime);
+            options.AddExpirationToken(expirationToken);
+
+            options.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration()
+            {
+                EvictionCallback = (key, value, reason, state) =>
+                {
+                    if (reason == EvictionReason.TokenExpired || reason == EvictionReason.Expired)
+                    {
+                        var newValue = invalidateConfigure(key);
+                        _memoryCache.Set(key, newValue ?? value, GetMemoryCacheEntryOptions(_refreshTimeInSeconds, invalidateConfigure));
+                    }
+                }
+            });
+
+            return options;
+        }
+    }
+}
---
+++ Examples/AspNetCoreCacheTest/MemoryCacheTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+namespace MemoryCacheTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //await TestExpireCacheAsync();
+            var cache = new MemoryCache(new MemoryCacheOptions()
+            {
+                ExpirationScanFrequency = new TimeSpan(0, 0, 1)
+            });
+            var autoRefreshCache = new AutoRefreshMemoryCache(cache, 2);
+            var key = "Key";
+            for (int i = 0; i < 1000; i++)
+            {
+                var value = await autoRefreshCache.GetAsync(key, x => Guid.NewGuid().ToString());
+                Console.WriteLine(value);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            Console.ReadKey();
+        }
+
+        public static async Task TestExpireCacheAsync()
+        {
+            var cache = new MemoryCache(new MemoryCacheOptions()
+            {
+                ExpirationScanFrequency = new TimeSpan(0, 0, 1)
+            });
+
+            var key = "Key";
+            var value = "Value";
+            var newValue = "New Value";
+
+
+            int expirationSeconds = 1;
+            var expirationTime = DateTime.Now.AddSeconds(expirationSeconds);
+            var expirationToken = new CancellationChangeToken(
+                new CancellationTokenSource(TimeSpan.FromSeconds(expirationSeconds + .01)).Token);
+
+            var options = new MemoryCacheEntryOptions();
+            options.SetAbsoluteExpiration(expirationTime);
+            options.AddExpirationToken(expirationToken);
+            options.PostEvictionCallbacks.Add(new PostEvictionCallbackRegistration()
+            {
+                EvictionCallback = (subKey, subValue, reason, state) =>
+                {
+                    if (reason == EvictionReason.TokenExpired || reason == EvictionReason.Expired)
+                    {
+                        cache.Set(key, newValue);
+                    }
+                }
+            });
+
+            cache.Set(key, value, options);
+            Console.WriteLine(cache.Get(key));
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            var result = cache.Get(key);
+            Console.WriteLine(result);
+        }
+    }
+}
