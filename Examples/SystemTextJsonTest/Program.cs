331576badc3169a1040c72b6156caf7579b23868
tuannguyenssu <tuannguyen@soongsil.ac.kr>
2021-09-24 11:08:03 +0700

    Added SystemTextJsonTest

---
+++ Examples/SystemTextJsonTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SystemTextJsonTest
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("age")]
+        public int? Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = @"{""Name"":""John"", ""age"":""34""}";
+            //var obj = JsonDocument.Parse(json);
+            //var name = obj.RootElement.GetProperty("Name").GetString();
+            //Console.WriteLine(name);
+
+            var options = new JsonSerializerOptions()
+            {
+                NumberHandling = JsonNumberHandling.AllowReadingFromString
+            };
+
+            var person = JsonSerializer.Deserialize<Person>(json, options);
+            Console.WriteLine(person.Name);
+        }
+    }
+}
